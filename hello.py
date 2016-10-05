11bb3fa1f74357189145d4d952d5f64b57fc1b36
gerardnaughton7 <gerardnaughton7@gmail.com>
2016-10-05 10:59:47 +0100

    step 4 add route name and take in variable

--- hello.py
+++ hello.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+    
+@app.route("/name/<name>")
+def name(name):
+    return "Your name is %s" % name
 
 if __name__ == "__main__":
     app.run()
