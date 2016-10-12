4bc197cdbac73cd99486820012bb100756371bca
gerardnaughton7 <gerardnaughton7@gmail.com>
2016-10-12 10:44:55 +0100

    step 5 and 6. sending out index.html and taking in info and returning query

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+# Gerard Naughton G00209309
+#webserver 
+#import flask
+#Origionally adapted from:
+# http://flask.pocoo.org/
+import flask as fl
+app = fl.Flask(__name__)
 
+#return index.html with form
 @app.route("/")
-def hello():
-    return "Hello World!"
-    
-@app.route("/name/<name>")
-def name(name):
-    return "Your name is %s" % name
+def root():
+    return app.send_static_file('index.html')
+
+#return the request hello and using get and post method take the name from the form in index.html   
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    return "Hello " + fl.request.form["name"] + "!"
 
 if __name__ == "__main__":
     app.run()
